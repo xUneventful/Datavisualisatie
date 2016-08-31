17adf3265619572114580e5de7503a7c3112cafc
Samantha Verbakel <smmverbakel@gmail.com>
2016-08-31 08:14:00 +0200

    added landingspage

--- index.php
+++ index.php
@@ -4,27 +4,11 @@
     <script type="text/javascript" src="drugs.js"></script>
   </head>
   <body>
-  <!--Table and divs that hold the pie charts-->
-    <table class="softDrugs">
-      <tr>
-        <td><div id="Alcohol_chart_div"></div></td>
-        <td><div id="Tobacco_chart_div"></div></td>
-        <td><div id="Marijuana_chart_div"></div></td>
-        <td><div id="Sedatives_chart_div"></div></td>
-      </tr>
-    </table>
-    
-    <table class="hardDrugs">
-      <tr>
-        <td><div id="Cocaine_chart_div"></div></td>
-        <td><div id="Crack_chart_div"></div></td>
-        <td><div id="Hallucinogen_chart_div"></div></td>
-        <td><div id="Heroin_chart_div"></div></td>
-        <td><div id="Inhalants_chart_div"></div></td>
-        <td><div id="PainRelievers_chart_div"></div></td>
-        <td><div id="Stimulants_chart_div"></div></td>
-        <td><div id="Tranquilizers_chart_div"></div></td>
-      </tr>
-    </table>
+  
+  <!-- randomly displayed image. -->
+  <img src=".../images/image_<?php $random = rand(1,n); echo $random; ?>.png" alt="[ Random Image ]" height="50" width="50" />
+  
+  <!-- show stats button -->
+  <a href="stats.php">Go to Google</a>
   </body>
 </html>
---
+++ stats.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="drugs.js"></script>
+  </head>
+  <body>
+
+ <!--Table and divs that hold the pie charts-->
+    <table class="softDrugs">
+      <tr>
+        <td><div id="Alcohol_chart_div"></div></td>
+        <td><div id="Tobacco_chart_div"></div></td>
+        <td><div id="Marijuana_chart_div"></div></td>
+        <td><div id="Sedatives_chart_div"></div></td>
+      </tr>
+    </table>
+    
+    <table class="hardDrugs">
+      <tr>
+        <td><div id="Cocaine_chart_div"></div></td>
+        <td><div id="Crack_chart_div"></div></td>
+        <td><div id="Hallucinogen_chart_div"></div></td>
+        <td><div id="Heroin_chart_div"></div></td>
+        <td><div id="Inhalants_chart_div"></div></td>
+        <td><div id="PainRelievers_chart_div"></div></td>
+        <td><div id="Stimulants_chart_div"></div></td>
+        <td><div id="Tranquilizers_chart_div"></div></td>
+      </tr>
+    </table>
+<body>
+</body>
+</html>
